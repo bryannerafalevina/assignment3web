68f0d8981ea175ea7427581124b63e5d4e002c39
RafaLevina06 <brafalevina06@gmail.com>
2024-02-27 15:55:56 +0700

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1> Enjoy Our</h1>
+      <h1> Delicious Meal</h1>
+    </div>
+      <h4>tempor erat elitr rebum at clita.Diam dolor diam ipsum sit.Aliqu diam amet diam et
+      eos. Clita erat ipsum et lorem et sit, sad stet lorem sit clita dui justo magna dolore erat amet</h4>
+      <div>
+        <router-link to="/book-a-table" custom v-slot="{ navigate }">
+          <button @click="navigate" role="link">
+            Book A Table
+          </button>
+        </router-link>
+  </div>
+  </div>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+  };
+  </script>
+
+<style scoped>
+.home {
+  background-color: #f8f9fa;
+  text-align: center;
+}
+
+.container {
+  max-width: 800px;
+  margin-top: 40px;
+}
+
+h1 {
+  font-size: 2.5em;
+  color:black;
+}
+
+p {
+  font-size: 1.2em;
+  color: #333;
+}
+
+</style>
---
+++ src/views/MenuView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ListMenu from '../components/ListMenu.vue';
+import Menu from '../components/Menu.vue'
+</script>
+
+<template>
+  <main>
+    <ListMenu />
+  </main>
+</template>
